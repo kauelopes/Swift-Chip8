c744b6b82b15f0e519df722ed90e835328f786ad
Kaue <kauelopesmoraes22@gmail.com>
2017-01-02 05:22:13 -0200

    conflict error

---
+++ chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/chip8/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  chip8
+//
+//  Created by Kaue Lopes de Moraes on 30/12/16.
+//  Copyright © 2016 kauelm. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
